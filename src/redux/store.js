a73934933cbc1e1bda8cb6ae1cd664c8ad843e30
Flaperon09 <kasyanenko.yuriy@gmail.com>
2024-06-20 20:45:58 +0300

    Удалено сохранение в LocalStore

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,45 +2,12 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filterSlice";
 
-import storage from 'redux-persist/lib/storage';
-import {
-    persistStore,
-    persistReducer,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-} from 'redux-persist';
-
-// Конфигурация persist
-const persistConfig = {
-    key: 'root',
-    storage,
-    whitelist: ['contacts'],
-};
-
 // Комбинированный редюсер контактов и фильтра
 const rootReducer = combineReducers({
     contacts: contactsReducer,
     filter: filterReducer,
 })
 
-// Сохранение контактов в локальном хранилище
-const persistedContactsReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-    reducer: persistedContactsReducer,
-    // Прослойка для игнорирования неиспользуемых экшенов
-    // (отсутствие этой прослойки вызывает ошибку приложения)
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-            },
-        }
-    ),
+    reducer: rootReducer,
 });
-
-export const persistor = persistStore(store)
